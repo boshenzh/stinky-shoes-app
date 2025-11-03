5f753450be7e7515038e6b1528b0ed3a728df0fa
boshenzh <boshenz@vt.edu>
2025-11-04 02:46:37 +0800

    ad

--- public/lib/constants.js
+++ public/lib/constants.js
@@ -87,7 +87,7 @@ export const MAP_CONFIG = {
   CLUSTER_RADIUS: 13, // Cluster radius in pixels
   CLUSTER_MIN_ZOOM: 5, // Start clustering at this zoom level (clustering enabled at/above this zoom)
   CIRCLE_MIN_ZOOM: 4, // Hide gym circles below this zoom level (desktop)
-  CIRCLE_MIN_ZOOM_MOBILE: 8, // Hide gym circles below this zoom level (mobile)
+  CIRCLE_MIN_ZOOM_MOBILE: 12, // Hide gym circles below this zoom level (mobile)
   POPUP_ZOOM: 12, // Zoom level when clicking on a marker (desktop)
   POPUP_ZOOM_MOBILE: 12, // Zoom level when clicking on a marker (mobile)
 };
